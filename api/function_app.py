9529682cebaf4b89072198f8e8f9501beb4c11e5
Muhammad Luqman Hakim <luqmansyahrulnizam@gmail.com>
2026-05-10 01:19:00 +0800

    feat: added test API endpoint

---
+++ api/function_app.py
@@ -0,0 +1,12 @@
+import azure.functions as func
+import json
+
+app = func.FunctionApp()
+
+@app.route(route="test", auth_level=func.AuthLevel.ANONYMOUS)
+def test(req: func.HttpRequest) -> func.HttpResponse:
+    return func.HttpResponse(
+        json.dumps({"message": "API is connected and talking to React!"}),
+        mimetype="application/json",
+        status_code=200
+    )
