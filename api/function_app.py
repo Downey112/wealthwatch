1bae066a891754f7bff9f0a3d5d698ceff5122c3
Muhammad Luqman Hakim <luqmansyahrulnizam@gmail.com>
2026-05-10 01:28:10 +0800

    fix: explicit routing config and anonymous auth level

--- api/function_app.py
+++ api/function_app.py
@@ -6,7 +6,7 @@ app = func.FunctionApp()
 @app.route(route="test", auth_level=func.AuthLevel.ANONYMOUS)
 def test(req: func.HttpRequest) -> func.HttpResponse:
     return func.HttpResponse(
-        json.dumps({"message": "API is connected and talking to React!"}),
+        json.dumps({"message": "API Bridge is Active!"}),
         mimetype="application/json",
         status_code=200
     )
